a15d7e3b4c8e09fdc03b89da744ea23c706195d6
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-22 18:18:08 +0100

    aggiunto overlay card

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,12 +4,17 @@
 {{-- collego lo yield con il contenuto di Homepage --}}
 @section('content')
   <div class="container">
-    <h1>Lista dei prodotti</h1>
+    <div class="title">
+      <h2>Le Lunghe</h2>
+
+    </div>
     <div class="card-container">
       @foreach ($pasta as $tipo_pasta)
         <div class="card">
           <img src="{{ $tipo_pasta['src']}}" alt="{{ $tipo_pasta['titolo'] }}">
-
+          <div class="overlay">
+            <a href="#">{{ $tipo_pasta['titolo'] }}</a>
+          </div>
 
         </div>
       @endforeach
