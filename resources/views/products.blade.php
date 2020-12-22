459d9919912caece3f7ab48b3d820255ba0306c0
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-22 17:52:02 +0100

    inserito card

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -5,7 +5,15 @@
 @section('content')
   <div class="container">
     <h1>Lista dei prodotti</h1>
+    <div class="card-container">
+      @foreach ($pasta as $tipo_pasta)
+        <div class="card">
+          <p>{{ $tipo_pasta['titolo'] }}</p>
 
+        </div>
+      @endforeach
+
+    </div>
   </div>
 
 @endsection
