74dd329f29945848a1b5ff1184e77c78766090e1
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-22 16:42:24 +0100

    aggiungo logo e menu e linkato pagine

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,30 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   </head>
   <body>
-    @yield('content')
+    <header>
+      <div class="logo">
+        <img src="{{ asset('images/logo-molisana.png') }}" alt="La Molisana - logo">
+      </div>
+      <div class="main-menu">
+        <ul>
+          <li>
+            <a href="{{ route('homepage') }}">Home</a>
+          </li>
+          <li>
+            <a href="{{ route('prodotti') }}">Prodotti</a>
+          </li>
+          <li>
+            <a href="#">News</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <main>
+      @yield('content')
+
+    </main>
+    <footer>
+      footer
+    </footer>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('homepage');
-});
+})->name('homepage');
 
 Route::get('/prodotti', function () {
     return view('products');
-});
+})->name('prodotti');
