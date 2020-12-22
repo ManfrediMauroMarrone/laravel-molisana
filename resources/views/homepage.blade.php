9a04a10ded28a9020097daaf466d354cabe7c9af
Manfredi Mauro Marrone <manfredi.marrone@hotmail.it>
2020-12-22 16:18:48 +0100

    creato layout pagina home e prodotti

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,9 @@
+{{-- collego il layout di app con Homepage --}}
+@extends('layouts.app')
+
+{{-- collego lo yield con il contenuto di Homepage --}}
+@section('content')
+  <h1>Homepage</h1>
+  <p>ciao</p>
+
+@endsection
---
+++ resources/views/products.blade.php
@@ -0,0 +1,8 @@
+{{-- collego il layout di app con Homepage --}}
+@extends('layouts.app')
+
+{{-- collego lo yield con il contenuto di Homepage --}}
+@section('content')
+  <h1>Lista dei prodotti</h1>
+
+@endsection
